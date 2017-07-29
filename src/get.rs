3fbe74bcc9930f874b2642c665c3d3c5c1e3a958
MPThLee <mpthlee@gmail.com>
2017-07-29 23:39:19 +0900

    Remove debug prints

--- src/get.rs
+++ src/get.rs
@@ -7,7 +7,6 @@ pub fn get_data(url: &str) -> Result<Value, ()> {
     let mut r = reqwest::get(url).unwrap();
     let mut d = String::new();
     r.read_to_string(&mut d).unwrap();
-    println!("{} / {}", url, d);
 
     let j: Value = serde_json::from_str(&d).unwrap();
     match j["status"].to_string().as_str() {
